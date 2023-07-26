51b588029cf927b7db6524eb909c0aba916a4715
Bartosz Kwiatkowski <kwiatkowsky.kontakt@gmail.com>
2023-07-26 12:41:02 +0200

    feat: implement error handling interceptor with logging in console log

---
+++ src/app/core/interceptors/error-interceptor.interceptor.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpEvent,
+  HttpInterceptor,
+  HttpHandler,
+  HttpRequest,
+  HttpErrorResponse,
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return next.handle(request).pipe(
+      catchError((error: HttpErrorResponse) => {
+        let errorMessage = 'An unknown error occurred';
+        let errorDetails = '';
+
+        if (error.error instanceof ErrorEvent) {
+          errorMessage = `Error: ${error.error.message}`;
+        } else {
+          errorMessage = `Error Code: ${error.status}`;
+          if (error.error && error.error.message) {
+            errorDetails = error.error.message;
+          }
+        }
+
+        const errorResponse = {
+          timestamp: new Date().toISOString(),
+          message: errorMessage,
+          details: errorDetails,
+        };
+
+        console.error(errorResponse);
+        return throwError(() => errorResponse);
+      })
+    );
+  }
+}
