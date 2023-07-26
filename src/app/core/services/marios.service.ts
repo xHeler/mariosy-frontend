1acd9dbcbbf8ff1f146cbab3201370c7d1cd025d
Bartosz Kwiatkowski <kwiatkowsky.kontakt@gmail.com>
2023-07-26 12:56:14 +0200

    feat: implement marios service logic

--- src/app/core/services/marios.service.ts
+++ src/app/core/services/marios.service.ts
@@ -1,9 +1,85 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, ReplaySubject, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { Marios } from '../models/marios.model';
+import { MariosList } from '../models/marios-list.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MariosService {
+  private baseUrl = '/api/marios';
+  private mariosData: Marios[] = [];
+  private mariosList$ = new ReplaySubject<Marios[]>(1);
+  private destroy$ = new Subject<void>();
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  get mariosList(): Observable<Marios[]> {
+    if (this.mariosData.length === 0) {
+      this.fetchMariosList();
+    }
+    return this.mariosList$.asObservable();
+  }
+
+  private fetchMariosList(): void {
+    this.http
+      .get<Marios[]>(this.baseUrl)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((data) => {
+        this.mariosData = [...data];
+        this.mariosList$.next(this.mariosData);
+      });
+  }
+
+  getMariosById(mariosId: string): Observable<Marios> {
+    return this.http
+      .get<Marios>(`${this.baseUrl}/${mariosId}`)
+      .pipe(takeUntil(this.destroy$));
+  }
+
+  addMarios(payload: Marios): void {
+    this.http
+      .post<Marios>(this.baseUrl, payload)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((data) => {
+        this.mariosData = [...this.mariosData, data];
+        this.mariosList$.next(this.mariosData);
+      });
+  }
+
+  updateMarios(marios: Marios): void {
+    this.http
+      .put<Marios>(`${this.baseUrl}/${marios.id}`, marios)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((data) => {
+        this.mariosData = this.mariosData.map((m) =>
+          m.id === data.id ? data : m
+        );
+        this.mariosList$.next(this.mariosData);
+      });
+  }
+
+  deleteMarios(mariosId: string): void {
+    this.http
+      .delete(`${this.baseUrl}/${mariosId}`)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(() => {
+        this.mariosData = this.mariosData.filter((m) => m.id !== mariosId);
+        this.mariosList$.next(this.mariosData);
+      });
+  }
+
+  getMariosSentByEmployee(employeeId: string): Observable<MariosList> {
+    return this.http
+      .get<MariosList>(`${this.baseUrl}/sent/${employeeId}`)
+      .pipe(takeUntil(this.destroy$));
+  }
+
+  getMariosReceivedByEmployee(employeeId: string): Observable<MariosList> {
+    return this.http
+      .get<MariosList>(`${this.baseUrl}/receive/${employeeId}`)
+      .pipe(takeUntil(this.destroy$));
+  }
 }
