4b090eeb46eb5041e022f02b50df3f5e83ad1ffa
Bartosz Kwiatkowski <kwiatkowsky.kontakt@gmail.com>
2023-07-27 14:08:47 +0200

    feat: add inputs: label and counter for info card

--- src/app/core/components/info-card/info-card.component.ts
+++ src/app/core/components/info-card/info-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-info-card',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./info-card.component.scss']
 })
 export class InfoCardComponent {
-  
+  @Input() public label = '';
+  @Input() public counter = 0;
 }
