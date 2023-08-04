67ca04a3297568f629397a7642a267315299fc46
Bartosz Kwiatkowski <kwiatkowsky.kontakt@gmail.com>
2023-08-04 13:18:37 +0200

    refactor: extract employee search to more methods

--- src/app/core/components/add-marios-form/components/employee-select/employee-select.component.ts
+++ src/app/core/components/add-marios-form/components/employee-select/employee-select.component.ts
@@ -5,7 +5,6 @@ import {
   EmployeeWithFullName,
 } from '../../../../models/employee.model';
 import { EmployeeService } from '../../../../services/employee.service';
-
 import { ReplaySubject } from 'rxjs';
 
 @Component({
@@ -23,21 +22,27 @@ export class EmployeeSelectComponent {
   onSearchChange(event: { term: string; items: any[] }) {
     this.employeeService.searchEmployees(event.term, true).subscribe(
       (data: Employee[]) => {
-        let employees: EmployeeWithFullName[] = data.map((employee) => ({
-          ...employee,
-          fullName: `${employee.firstName} ${employee.lastName}`,
-        }));
-
-        employees = employees.filter(
-          (employee) => !this.existsInSelected(employee)
-        );
-
-        this.employees$.next(employees);
+        const employees: EmployeeWithFullName[] = this.mapEmployees(data);
+        const filteredEmployees = this.filterSelectedEmployees(employees);
+        this.employees$.next(filteredEmployees);
       },
       (error) => {
         console.error('Error while fetching data:', error);
       }
     );
+  }
+
+  private mapEmployees(data: Employee[]): EmployeeWithFullName[] {
+    return data.map((employee) => ({
+      ...employee,
+      fullName: `${employee.firstName} ${employee.lastName}`,
+    }));
+  }
+
+  private filterSelectedEmployees(
+    employees: EmployeeWithFullName[]
+  ): EmployeeWithFullName[] {
+    return employees.filter((employee) => !this.existsInSelected(employee));
   }
 
   private existsInSelected(employee: EmployeeWithFullName): boolean {
