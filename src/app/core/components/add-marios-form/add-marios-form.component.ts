19b777c7db697d1725973cc44f76dec2998f626d
Bartosz Kwiatkowski <kwiatkowsky.kontakt@gmail.com>
2023-08-04 13:16:57 +0200

    refactor: extract on submit to more methods

--- src/app/core/components/add-marios-form/add-marios-form.component.ts
+++ src/app/core/components/add-marios-form/add-marios-form.component.ts
@@ -1,8 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Employee } from '../../models/employee.model';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MariosService } from '../../services/marios.service';
 import { Router } from '@angular/router';
+
+import { Employee } from '../../models/employee.model';
 
 @Component({
   selector: 'app-add-marios-form',
@@ -12,6 +13,7 @@ import { Router } from '@angular/router';
 export class AddMariosFormComponent implements OnInit {
   @Input() public maxTitleLength = 128;
   @Input() public maxMessageLength = 255;
+
   form: FormGroup = new FormGroup({});
 
   constructor(
@@ -28,42 +30,52 @@ export class AddMariosFormComponent implements OnInit {
     this.form = this.formBuilder.group({
       receiversId: [''],
       reaction: '',
-      title: [
-        '',
-        [
-          Validators.required,
-          Validators.minLength(1),
-          Validators.maxLength(this.maxTitleLength),
-        ],
-      ],
-      message: [
-        '',
-        [
-          Validators.required,
-          Validators.minLength(1),
-          Validators.maxLength(this.maxMessageLength),
-        ],
-      ],
+      title: this.createFormControlWithValidation('', [
+        Validators.required,
+        Validators.minLength(1),
+        Validators.maxLength(this.maxTitleLength),
+      ]),
+      message: this.createFormControlWithValidation('', [
+        Validators.required,
+        Validators.minLength(1),
+        Validators.maxLength(this.maxMessageLength),
+      ]),
     });
   }
 
+  private createFormControlWithValidation(initialValue: any, validators: any) {
+    return this.formBuilder.control(initialValue, validators);
+  }
+
   onSubmit() {
+    this.extractEmployeeIds();
+    if (this.form.valid) {
+      this.addMarios();
+    } else {
+      this.markFormControlsAsTouched();
+    }
+  }
+
+  private extractEmployeeIds() {
     this.form.value.receiversId = this.form.value.receiversId.map(
       (employee: Employee) => employee.id
     );
-    if (this.form.valid) {
-      this.mariosService.addMarios(this.form.value).subscribe(
-        () => {
-          this.router.navigate(['/home']);
-        },
-        (error) => {
-          console.error('Error while adding Marios:', error);
-        }
-      );
-    } else {
-      Object.keys(this.form.controls).forEach((key) => {
-        this.form.get(key)?.markAsTouched();
-      });
-    }
+  }
+
+  private addMarios() {
+    this.mariosService.addMarios(this.form.value).subscribe(
+      () => {
+        this.router.navigate(['/home']);
+      },
+      (error) => {
+        console.error('Error while adding Marios:', error);
+      }
+    );
+  }
+
+  private markFormControlsAsTouched() {
+    Object.keys(this.form.controls).forEach((key) => {
+      this.form.get(key)?.markAsTouched();
+    });
   }
 }
