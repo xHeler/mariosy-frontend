574e976fa7e3cbc23d1f1068c36ba45837238201
Bartosz Kwiatkowski <kwiatkowsky.kontakt@gmail.com>
2023-08-07 14:45:14 +0200

    test: testing adding marios

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,4 @@
 describe('My First Test', () => {
-
   beforeEach(() => {
     cy.viewport(1920, 1080);
   });
@@ -7,53 +6,71 @@ describe('My First Test', () => {
   // testing routing
 
   it('Redirect from home page to received marios', () => {
-    cy.visit('/')
+    cy.visit('/');
     cy.contains('RECEIVED MARIOS:').click();
     cy.url().should('include', '/received-marios');
-  })
+  });
 
   it('Redirect from home page to sent marios', () => {
-    cy.visit('/')
+    cy.visit('/');
     cy.contains('SENT MARIOS:').click();
     cy.url().should('include', '/sent-marios');
-  })
+  });
 
   it('Redirect from home page to add marios', () => {
-    cy.visit('/')
+    cy.visit('/');
     cy.contains('ADD MARIOS').click();
     cy.url().should('include', '/add-marios');
-  })
+  });
 
   it('Redirect from add marios page to home', () => {
-    cy.visit('/add-marios')
+    cy.visit('/add-marios');
     cy.contains('BACK').click();
     cy.url().should('include', '/');
-  })
+  });
 
   it('Redirect from sent marios page to home', () => {
-    cy.visit('/sent-marios')
+    cy.visit('/sent-marios');
     cy.contains('BACK').click();
     cy.url().should('include', '/');
-  })
+  });
 
   it('Redirect from received marios page to home', () => {
-    cy.visit('/received-marios')
+    cy.visit('/received-marios');
     cy.contains('BACK').click();
     cy.url().should('include', '/');
-  })
+  });
 
   // testing home page
   it('Clicking and closing modal', () => {
-    cy.visit('/')
+    cy.visit('/');
     cy.contains('From:').click();
 
     cy.get('.mat-mdc-dialog-container').should('be.visible');
 
-    cy.get('.container-fluid.dialog-container')
-    .find('button')
-    .click();
+    cy.get('.container-fluid.dialog-container').find('button').click();
     cy.get('.mat-mdc-dialog-container').should('be.visible');
-  })
+  });
 
+  // testing add marios
+  it('Add example marios', () => {
+    const title = 'Add example marios';
+    const message = 'Create example message';
 
-})
+    cy.visit('/add-marios');
+
+    cy.get('.ng-select-container').find('input').type('Adam');
+    cy.get('.ng-option-marked').click();
+
+    cy.contains(' Good job! ').click();
+    cy.get('app-title-text-area').find('textarea').type(title);
+
+    cy.get('app-comment-text-area').find('textarea').type(message);
+
+    cy.contains('SEND').click();
+    cy.url().should('include', '/home');
+
+    cy.contains(message).click();
+    cy.get('.mat-mdc-dialog-container').should('be.visible');
+  });
+});
