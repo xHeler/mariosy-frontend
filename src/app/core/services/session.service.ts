8cc482d7a72a255ea0dc68de5aa7e327c2b52f19
Bartosz Kwiatkowski <kwiatkowsky.kontakt@gmail.com>
2023-08-16 15:00:54 +0200

    feat: integrate keycloak with angular

--- src/app/core/services/session.service.ts
+++ src/app/core/services/session.service.ts
@@ -1,16 +1,36 @@
 import { Injectable } from '@angular/core';
+import { KeycloakService } from "keycloak-angular";
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SessionService {
-  private userUUID = "95f1b246-81ff-43a6-92c6-98a208abe331";
+  private userUUID$: BehaviorSubject<string | null> = new BehaviorSubject<string | null>(null);
+
+  constructor(private keycloakService: KeycloakService) {}
+
+  async initializeService(): Promise<void> {
+    await this.extractAndSetUUID();
+  }
+
+  private async extractAndSetUUID(): Promise<void> {
+    try {
+      const userProfile = await this.keycloakService.loadUserProfile();
+
+      if (userProfile && userProfile.id) {
+        this.setUserUUID(userProfile.id);
+      }
+    } catch (error) {
+      console.error('Error loading user profile:', error);
+    }
+  }
 
   setUserUUID(uuid: string): void {
-    this.userUUID = uuid;
+    this.userUUID$.next(uuid);
   }
 
   getUserUUID(): string | null {
-    return this.userUUID;
+    return this.userUUID$.getValue();
   }
 }
--- src/app/init/keycloak-init.factory.ts
+++ src/app/init/keycloak-init.factory.ts
@@ -1,19 +1,25 @@
 import { KeycloakService } from "keycloak-angular";
+import { SessionService } from './../core/services/session.service';
 
 export function initializeKeycloak(
-  keycloak: KeycloakService
-  ) {
-    return () =>
-      keycloak.init({
-        config: {
-          url: 'http://localhost:8080',
-          realm: 'deloitte',
-          clientId: 'mariosy-frontend',
-        },
-        initOptions: {
-          onLoad: 'check-sso',
-          silentCheckSsoRedirectUri:
-            window.location.origin + '/assets/verify-sso.html'
-        }
-      });
+  keycloak: KeycloakService,
+  sessionService: SessionService
+): () => Promise<void> {
+  return async () => {
+    await keycloak.init({
+      config: {
+        url: 'http://localhost:8080',
+        realm: 'deloitte',
+        clientId: 'mariosy-frontend',
+      },
+      initOptions: {
+        onLoad: 'check-sso',
+        silentCheckSsoRedirectUri:
+          window.location.origin + '/assets/verify-sso.html',
+      },
+      
+    });
+
+    await sessionService.initializeService();
+  };
 }
