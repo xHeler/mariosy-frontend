1bea625b80f588a00ff8c5ce20d3d488d4d434fc
Bartosz Kwiatkowski <kwiatkowsky.kontakt@gmail.com>
2023-08-23 13:28:58 +0200

    feature: change reaction star color

--- src/app/core/components/marios-card/marios-card.component.ts
+++ src/app/core/components/marios-card/marios-card.component.ts
@@ -3,6 +3,7 @@ import { Marios } from '../../models/marios.model';
 import { Employee } from '../../models/employee.model';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { MariosDialogComponent } from '../marios-dialog/marios-dialog.component';
+import { MariosReaction } from '../../enums/marios-reaction.enum';
 
 @Component({
   selector: 'app-marios-card',
@@ -35,5 +36,24 @@ export class MariosCardComponent {
     };
 
     this.dialog.open(MariosDialogComponent, dialogConfig);
+  }
+
+  getBackgroundImage(mariosReaction: MariosReaction): string {
+    switch (mariosReaction) {
+      case MariosReaction.THANK_YOU:
+        return '/assets/images/stars/THANK_YOU.svg';
+      case MariosReaction.GOOD_JOB:
+        return '/assets/images/stars/GOOD_JOB.svg';
+      case MariosReaction.IMPRESSIVE:
+        return '/assets/images/stars/IMPRESSIVE.svg';
+      case MariosReaction.EXCEPTIONAL:
+        return '/assets/images/stars/EXCEPTIONAL.svg';
+      case MariosReaction.AWESOME:
+        return '/assets/images/stars/AWESOME.svg';
+      case MariosReaction.OUTSTANDING:
+        return '/assets/images/stars/OUTSTANDING.svg';
+      default:
+        return '';
+    }
   }
 }
